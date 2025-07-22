c0f65fe355c9dd4855a3a1b5d407a42a34fb0302
Jagan V Jinil <jaganvjinil@gmail.com>
2025-07-22 13:08:17 +0530

    Add static images and update background for URL list

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),                     # login at root
     path('signup/', views.signup_view, name='signup'),
-    path('', views.url_list, name='url_list'),
+    path('urllist/', views.url_list, name='url_list'),            # url_list moved
     path('logout/', views.logout_view, name='logout'),
     path('add/', views.add_url, name='add_url'),
     path('edit/<int:pk>/', views.edit_url, name='edit_url'),
     path('delete/<int:pk>/', views.delete_url, name='delete_url'),
-    path('<str:short_code>/', views.redirect_short_url, name='redirect_short_url'), 
+    path('<str:short_code>/', views.redirect_short_url, name='redirect_short_url'),
     path('accounts/login/', views.login_view),
 ]
